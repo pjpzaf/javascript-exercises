80a4474f73e40ea2b205466ecb88161da51be194
pjpzaf <paolojosemari.zafra@gmail.com>
2022-09-14 20:09:52 +0800

    Edited main JS script file based on problem specs

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,25 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+    
+    let newString;
 
+    if (num===0) {
+        newstring = "\" \"";
+    }
+    else if (num>0) {
+        newString = string;
+
+        if (num!=1) {
+            mainLoop: for (let i=1 ;i<num ;i++) {
+                newString = newString + string;
+            }
+        }
+    }
+    else {
+        newString = "ERROR";
+    }
+        
+
+return newString;
 };
 
 // Do not edit below this line
