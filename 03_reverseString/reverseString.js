6f47bb2a25b8bc97a88b44b16299521bc6d0e7c4
pjpzaf <paolojosemari.zafra@gmail.com>
2022-09-15 18:45:52 +0800

    added code in js file to match problem specs

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,28 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    let arrayString = [];
+    let stringReversed;
+
+    //checks if input is an empty string. skips the two for loops that follow if truthy
+    if (inputString === ""){
+        stringReversed = "";
+    }
+
+    //extracts characters from input string in reverse order and saves it in an array
+    for (let i = 0; i<inputString.length; i++){
+            arrayString[i] = inputString.charAt((inputString.length - 1) - i);
+    };
+
+    //assembling the original text but in reverse order
+    for (let i=0;i<arrayString.length;i++){
+        if (i===0){
+            stringReversed = arrayString[i];
+        }
+        else {
+            stringReversed = stringReversed + arrayString[i];
+        }
+    }
+
+    return stringReversed;
 
 };
 
