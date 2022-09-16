abf85b461f7304bb1ce3e7c3e6fc37e892659693
pjpzaf <paolojosemari.zafra@gmail.com>
2022-09-16 18:49:09 +0800

    added code in js file based on problem specs

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,30 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputAllObjects) {
 
+    //merges all inputs in the function to one object and extrance the first element
+    let allInputs = Array.from(arguments);
+    let inputObjects = allInputs[0];
+
+    //to create objects to be removed in first element
+    let elementsToRemove = [];
+    let k = 0;
+    for (let i=1;i<allInputs.length;i++) {
+        elementsToRemove[k] = allInputs[i];
+        k++;
+    };
+
+    //delete elements from inputObjects that are in elementsToRemove
+    for(let element of elementsToRemove) {
+        for (let elementInInput of inputObjects) {
+            if (element === elementInInput) {
+                inputObjects.splice(inputObjects.indexOf(elementInInput),1);
+                break;
+            }
+            else {
+                continue;
+            }
+        };
+    };
+    return inputObjects;
 };
 
 // Do not edit below this line
