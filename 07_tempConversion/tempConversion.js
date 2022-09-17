dd3e6d39290ebdfce838ba8d05354478689a7395
pjpzaf <paolojosemari.zafra@gmail.com>
2022-09-18 05:26:15 +0800

    added js code based on problem specs

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const ftoc = function() {
+const ftoc = function(convertToCelsius) {
 
+  let tempInCelsius = (convertToCelsius-32)*(5/9);
+      tempInCelsius = Math.round(tempInCelsius*10)/10;    
+      tempInCelsius = parseFloat(tempInCelsius.toFixed(1));
+
+      return tempInCelsius;
 };
 
-const ctof = function() {
+const ctof = function(convertToFahrenheit) {
+  let tempInFahrenheit = (convertToFahrenheit*9/5) + 32;
+  tempInFahrenheit = Math.round(tempInFahrenheit*10)/10;    
+  tempInFahrenheit = parseFloat(tempInFahrenheit.toFixed(1));
+
+  return tempInFahrenheit;
 
 };
 
