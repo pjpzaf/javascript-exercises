b645711ab9ffd2a00c0af69c45d8be0f165af41f
pjpzaf <paolojosemari.zafra@gmail.com>
2022-09-18 05:00:36 +0800

    added js code based on problem specs

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,21 @@
-const leapYears = function() {
+const leapYears = function(inputYear) {
+
+    if (inputYear%4!=0) {
+        isLeapYear = false;
+    }
+    else if (inputYear%4===0) {
+        if (inputYear%100===0 && inputYear%400!=0) {
+            isLeapYear = false;
+        }
+        else if (inputYear%100===0 && inputYear%400===0) {
+            isLeapYear = true;
+        }
+        else {
+            isLeapYear = true;
+        }
+    }
+
+    return isLeapYear;
 
 };
 
