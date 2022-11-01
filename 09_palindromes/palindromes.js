d39f31f0bd020dc85151d1f1dd2d909230e869ae
pjpzaf <paolojosemari.zafra@gmail.com>
2022-11-02 07:03:31 +0800

    provided code for raw js file to match problem specs

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (inputString) {
 
-};
+    let inputWord = inputString.toLowerCase().replaceAll(",","").replaceAll(".","").replaceAll("!","").replaceAll(" ","").replaceAll("?","");
+    let outputBool;
+
+    inputArray = Array.from(inputWord);
+    processArray = Array.from(inputWord);
+    processArray.reverse();
+    
+    for (let i=0;i<inputArray.length-1;i++) {
+        if (inputArray[i]===processArray[i]) {
+            continue;
+        }
+        else if (inputArray[i]!==processArray[i]) {
+            return outputBool = false;
+        } 
+    }
+    return outputBool = true;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
