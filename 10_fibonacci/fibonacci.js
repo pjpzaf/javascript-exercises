da6d87bda80fdaac6cf4af87ad2cd3d299775d63
pjpzaf <paolojosemari.zafra@gmail.com>
2022-11-04 18:49:06 +0800

    added code block for fibonacci search function based on problem specs

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
-
+const fibonacci = function(inputNumber) {
+    let output; 
+    let processArray = [];
+    if (inputNumber>0 && (typeof (+inputNumber))==='number') {
+        for(let i=0;i<(+inputNumber);i++) {
+            if (i===0 || i===1) {
+                processArray[i] = 1;
+                if ((+inputNumber)===1 || (+inputNumber)===2){
+                    return output = processArray[i];
+                }
+            }
+            else if (i!==0 && i!==1) {
+                processArray[i] = processArray[i-1] + processArray[i-2];
+                if (i===(+inputNumber-1)){
+                    return output = processArray[i];
+                }
+            }
+        }
+    }
+    else {
+        return output="OOPS";
+    }
+        
 };
 
 // Do not edit below this line
