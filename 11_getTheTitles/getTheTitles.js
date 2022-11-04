f9427c13654e62ee2a68a28ed3d921de6f7eb184
pjpzaf <paolojosemari.zafra@gmail.com>
2022-11-04 19:09:05 +0800

    added .js codeblock based on problem specs

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const listOfTitles = books.map(books => books["title"]);
+    return listOfTitles;
 };
 
 // Do not edit below this line
