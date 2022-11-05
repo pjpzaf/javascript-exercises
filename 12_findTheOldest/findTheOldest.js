b35875db4f6023afec360b52b5488c35d6a3873b
pjpzaf <paolojosemari.zafra@gmail.com>
2022-11-06 07:38:26 +0800

    added code to .js file based on problem specs

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+const today = new Date();
+for (person of people) {
+    if (person.yearOfDeath===undefined) {
+        person.age = today.getFullYear()-person.yearOfBirth;
+    }
+    else {
+        person.age = person.yearOfDeath-person.yearOfBirth;
+    }
+}
 
-};
+const sortedPeople = people.sort((a, b) => (a.age) > (b.age) ? -1:1)
+    return sortedPeople[0];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
